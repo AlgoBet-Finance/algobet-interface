e2fd0662ae6c69b02abf5000de165809fca5d650
tidusronin1 <tidusronin1@gmail.com>
2022-11-20 00:17:11 +0700

    news

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -3,38 +3,26 @@ import $ from 'jquery'
 import { get } from 'services/api'
 
 export default function News() {
+  const [listNews, setListNews] = useState([] as any[])
   useEffect(() => {
-    const widthCard = $('.card-news-div')[0].offsetWidth + 24
-    $('#scroll-left-news').on('click', () => {
-      $('#scroll-bar-news').animate({ scrollLeft: ($('#scroll-bar-news').scrollLeft() || 0) - widthCard }, 300)
-    })
-    $('#scroll-right-news').on('click', () => {
-      $('#scroll-bar-news').animate({ scrollLeft: ($('#scroll-bar-news').scrollLeft() || 0) + widthCard }, 300)
-    })
-  }, [])
-  const list = [1, 2, 3, 4]
-  const [listNews, setListNews] = useState(list)
-  useEffect(() => {
-    get("http://api.algobet-sports.com/api/news", {
+    get('http://api.algobet-sports.com/api/news', {
       limit: 5,
       page: 1,
     }).then((response) => {
-      console.log(response,"sss")
-       
+      setListNews(response.data.news)
     })
   }, [])
   useEffect(() => {
-    // fetch(`http://api.algobet-sports.com/api/news?limit=2&page=1`)
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //  console.log(data)
-    //   })
-    //   .catch((err) => {
-    //     console.log("Error Reading data 212" + err);
-    //   });
-  }, []);
+    if (listNews) {
+      const widthCard = ($('.card-news-div') && $('.card-news-div')[0] ? $('.card-news-div')[0].offsetWidth : 400) + 24
+      $('#scroll-left-news').on('click', () => {
+        $('#scroll-bar-news').animate({ scrollLeft: ($('#scroll-bar-news').scrollLeft() || 0) - widthCard }, 300)
+      })
+      $('#scroll-right-news').on('click', () => {
+        $('#scroll-bar-news').animate({ scrollLeft: ($('#scroll-bar-news').scrollLeft() || 0) + widthCard }, 300)
+      })
+    }
+  }, [listNews])
   return (
     <div className="news ">
       <div className="container">
@@ -42,7 +30,7 @@ export default function News() {
           <h2>Featured news</h2>
           <div className="group-view-btn">
             <button type="button" className="btn-view btn-view-all">
-              <a href="/">VIEW ALL</a>
+              <a href="/news">VIEW ALL</a>
             </button>
             <button id="scroll-left-news" type="button" className="btn-view btn-prev-next btn-prev">
               <img src="/images/arrow-left.svg" alt="arrow-left" />
@@ -54,23 +42,20 @@ export default function News() {
         </div>
         <div className="row card-list" id="scroll-bar-news">
           {listNews.map((item) => (
-            <div className="col-md-4 card-news-div">
+            <div key={item.id} className="col-md-4 card-news-div">
               <div className="card-news">
-                <img src="/images/news/news-example.png" alt="news" />
+                <img src={item.urlToImage} alt="news" />
                 <div className="content-news">
-                  <div className="title-card">Lorem ipsum dolor sit amet</div>
-                  <div className="des-news">
-                    Enim laboris ullamco quis ullamco proident occaecat et deserunt eiusmod pariatur aliquip nulla minim
-                    officiaEnim laboris ullamco quis ullamco proident occaecat et deserunt eiusmod pariatur aliquip
-                    nulla minim officiaEnim laboris ullamco quis ullamco proident occaecat et deserunt eiusmod pariatur
-                    aliquip nulla minim officia.
-                  </div>
-                  <div className="readmore-news">
-                    <a target="_blank" href="/">
-                      READ MORE
-                    </a>
-                  </div>
+                  <div className="title-card">{item.title}</div>
+                  <div className="des-news">{item.description}</div>
                 </div>
+                <div style={{ marginTop: 'auto' }}>
+                    <div className="readmore-news">
+                      <a target="_blank" href={item.url} rel="noreferrer">
+                        READ MORE
+                      </a>
+                    </div>
+                  </div>
               </div>
             </div>
           ))}
